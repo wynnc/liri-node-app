c104409cb6e6a67d88fafd22564ef23e3b2ad8f7
Wynn Chaney <wynn.chaney@wanco.com>
2019-06-26 21:34:28 -0600

    movie-this functionality working.

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+var axios = require("axios");
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
@@ -12,11 +13,49 @@ var userInput = process.argv.slice(3).join("+");
 var action = process.argv[2];
 
 function movieThis(information){
+    
     if(information === ""){
         information = "Mr. Nobody";
         // console.log()
     }
-    console.log(`You are in the movieThis ${information}`)
+    var queryUrl = "http://www.omdbapi.com/?t=" + information + "&y=&plot=short&apikey=trilogy";
+    // console.log(`You are in the movieThis ${information}`)
+
+    axios.get(queryUrl).then(
+        function (response) {
+            var data = response.data
+            // * Title of the movie.
+            // * Year the movie came out.
+            // * IMDB Rating of the movie.
+            // * Rotten Tomatoes Rating of the movie.
+            // * Country where the movie was produced.
+            // * Language of the movie.
+            // * Plot of the movie.
+            // * Actors in the movie.
+            // console.log(response.data);
+            console.log(`Movie Title: ${data.Title}\n Year Released: ${data.Year}\n IMDB Rating: ${data.imdbRating}\n Rotten Tomatoes Rating: ${data.Ratings[1].Value}\n Country: ${data.Country}\n Language: ${data.Language}\n Plot: ${data.Plot}\n Actors: ${data.Actors}`)
+           
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 }
 
 function concertThis(information){
@@ -42,7 +81,7 @@ function doWhat(){
         if(error){
             return console.log(error);
         }
-        console.log(data);
+        // console.log(data);
         var dataArr = data.split(",");
         var action = dataArr[0];
         var userInput = dataArr[1];
